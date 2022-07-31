bab538de4b2a9702c06fdaa1e6d1f1e8ed7e9f9f
MyNameTsThad <pvpthadgaming@gmail.com>
2022-07-31 20:09:01 +0700

    idk

--- src/main/java/com/thaddev/coolideas/mixins/LivingEntityMixin.java
+++ src/main/java/com/thaddev/coolideas/mixins/LivingEntityMixin.java
@@ -13,19 +13,28 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "applyDamage", at = @At("HEAD"))
-    public void applyDamage(DamageSource source, float amount, CallbackInfo ci) {
+    @Inject(method = "damage", at = @At("TAIL"))
+    public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity thisEntity = (LivingEntity) (Object) this;
         if (!thisEntity.isInvulnerableTo(source)) {
             StatusEffectInstance effect;
             if ((effect = thisEntity.getStatusEffect(EffectInit.VULNERABILITY)) != null) {
                 int amplifier = Math.min(effect.getAmplifier() + 1, 4);
                 int toReduce = amplifier > 3 ? ((amplifier - 1) * 2) + 3 : amplifier * 2;
-                thisEntity.timeUntilRegen = 20 - toReduce;
+                thisEntity.timeUntilRegen = 1;
+                //idk why this isnt working
             }
+        }
+    }
+
+    @Inject(method = "applyDamage", at = @At("HEAD"))
+    public void applyDamage(DamageSource source, float amount, CallbackInfo ci) {
+        LivingEntity thisEntity = (LivingEntity) (Object) this;
+        if (!thisEntity.isInvulnerableTo(source)) {
             if ((source.getSource() instanceof DiamondHeadedArrow || source.getSource() instanceof ShortBowArrow) && source.getAttacker() instanceof ServerPlayerEntity player) {
                 player.world.playSound(null, player.getPos().x, player.getPos().y, player.getPos().z, SoundEvents.ENTITY_ARROW_HIT_PLAYER, SoundCategory.PLAYERS, 0.3F, 0.5F);
             }
