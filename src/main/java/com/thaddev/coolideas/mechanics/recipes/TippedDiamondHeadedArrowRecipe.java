b29e89c702fc54e8185a5e24adacbad853e9b512
MyNameTsThad <pvpthadgaming@gmail.com>
2022-08-01 20:26:26 +0700

    recipe stuff fixed

--- src/main/java/com/thaddev/coolideas/mechanics/recipes/TippedDiamondHeadedArrowRecipe.java
+++ src/main/java/com/thaddev/coolideas/mechanics/recipes/TippedDiamondHeadedArrowRecipe.java
@@ -1,20 +1,14 @@
 package com.thaddev.coolideas.mechanics.recipes;
 
-import com.google.gson.JsonObject;
 import com.thaddev.coolideas.mechanics.inits.ItemInit;
 import com.thaddev.coolideas.mechanics.inits.RecipeSerializerInit;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.potion.PotionUtil;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.recipe.RecipeType;
-import net.minecraft.recipe.ShapedRecipe;
 import net.minecraft.recipe.SpecialCraftingRecipe;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
 import net.minecraft.world.World;
 
 public class TippedDiamondHeadedArrowRecipe extends SpecialCraftingRecipe {
@@ -61,46 +55,5 @@ public class TippedDiamondHeadedArrowRecipe extends SpecialCraftingRecipe {
     @Override
     public RecipeSerializer<?> getSerializer() {
         return RecipeSerializerInit.TIPPED_DIAMOND_HEADED_ARROW;
-    }
-
-    public static class Type implements RecipeType<TippedDiamondHeadedArrowRecipe> {
-        // Define ExampleRecipe.Type as a singleton by making its constructor private and exposing an instance.
-        private Type() {
-        }
-
-        public static final Type INSTANCE = new Type();
-
-        // This will be needed in step 4
-        public static final String ID = "crafting_special_tipped_diamond_headed_arrow";
-    }
-
-    @Override
-    public RecipeType<?> getType() {
-        return Type.INSTANCE;
-    }
-
-    public static class Serializer implements RecipeSerializer<TippedDiamondHeadedArrowRecipe> {
-        @Override
-        public TippedDiamondHeadedArrowRecipe read(Identifier identifier, JsonObject jsonObject) {
-            Ingredient ingredient = Ingredient.fromJson(JsonHelper.getObject(jsonObject, "base"));
-            Ingredient ingredient2 = Ingredient.fromJson(JsonHelper.getObject(jsonObject, "addition"));
-            ItemStack itemStack = ShapedRecipe.outputFromJson(JsonHelper.getObject(jsonObject, "result"));
-            return new TippedDiamondHeadedArrowRecipe(identifier, ingredient, ingredient2, itemStack);
-        }
-
-        @Override
-        public TippedDiamondHeadedArrowRecipe read(Identifier identifier, PacketByteBuf packetByteBuf) {
-            Ingredient ingredient = Ingredient.fromPacket(packetByteBuf);
-            Ingredient ingredient2 = Ingredient.fromPacket(packetByteBuf);
-            ItemStack itemStack = packetByteBuf.readItemStack();
-            return new TippedDiamondHeadedArrowRecipe(identifier, ingredient, ingredient2, itemStack);
-        }
-
-        @Override
-        public void write(PacketByteBuf packetByteBuf, TippedDiamondHeadedArrowRecipe smithingRecipe) {
-            smithingRecipe.base.write(packetByteBuf);
-            smithingRecipe.addition.write(packetByteBuf);
-            packetByteBuf.writeItemStack(smithingRecipe.result);
-        }
     }
 }
