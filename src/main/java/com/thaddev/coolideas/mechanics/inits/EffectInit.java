6702f56a8217d4347a7a77483e9b10881915852c
MyNameTsThad <pvpthadgaming@gmail.com>
2022-08-02 20:14:56 +0700

    Version 1.0.6-forge-119 - Bump version just to match 1.18.2 port - Made Vulnerbility effect more red

--- src/main/java/com/thaddev/coolideas/mechanics/inits/EffectInit.java
+++ src/main/java/com/thaddev/coolideas/mechanics/inits/EffectInit.java
@@ -5,8 +5,6 @@ import com.thaddev.coolideas.content.effects.VulnerabilityEffect;
 import com.thaddev.coolideas.util.ColorUtils;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,5 +13,5 @@ public class EffectInit {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, CoolIdeasMod.MODID);
 
     public static final RegistryObject<MobEffect> VULNERABILITY = MOB_EFFECTS.register("vulnerability",
-            () -> new VulnerabilityEffect(MobEffectCategory.HARMFUL, ColorUtils.rgbToInteger(10, 0, 0)));
+        () -> new VulnerabilityEffect(MobEffectCategory.HARMFUL, ColorUtils.rgbToInteger(50, 0, 0)));
 }
