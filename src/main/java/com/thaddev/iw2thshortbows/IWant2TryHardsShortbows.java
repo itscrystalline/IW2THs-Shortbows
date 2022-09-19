6a8ecffada52da4253adf693fdd5d10aee7a5490
MyNameTsThad <pvpthadgaming@gmail.com>
2022-09-19 12:07:39 +0700

    Version 1.1.1-patch1-fabric-119 - Made the earlyrisers work

--- src/main/java/com/thaddev/iw2thshortbows/IWant2TryHardsShortbows.java
+++ src/main/java/com/thaddev/iw2thshortbows/IWant2TryHardsShortbows.java
@@ -21,7 +21,7 @@ public class IWant2TryHardsShortbows implements ModInitializer {
 	public static IWant2TryHardsShortbows instance;
 	public static IWant2TryHardsShortbowsClient client;
 
-	public static String VERSION = "1.1.1";
+	public static String VERSION = "1.1.1-patch1";
 
 	public static final String MESSAGE_WELCOME = "message.iw2thshortbows.welcome";
 	public static final String SCREEN_VERSION_MISMATCH = "menu.iw2thshortbows.modmismatch";
